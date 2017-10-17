9ad423494e20561a79ee6ef9bb6cd47770961759
Chandra Kellwan <Chandra@Chandras-MacBook-Pro.local>
2017-10-16 20:30:16 -0400

    changed folder structure

--- json-web-token/index.js
+++ json-web-token/index.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const passport = require('passport');
-const config = require('./config');
+const express 		= require('express');
+const bodyParser 	= require('body-parser');
+const passport 		= require('passport');
+const config 		= require('./config');
 
 // connect to the database and load models
 require('./server/models').connect(config.dbUri);
