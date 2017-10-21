c5979063dab348ebd186da37366b464b72e1d2e5
Chandra Kellwan <Chandra@Chandras-MacBook-Pro.local>
2017-10-21 09:15:02 -0400

    changed PORT in index.js

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express 		= require('express');
 const bodyParser 	= require('body-parser');
 const passport 		= require('passport');
 const config 		= require('./config');
+var PORT = process.env.PORT || 3000;
+
 
 // connect to the database and load models
 require('./server/models').connect(config.dbUri);
@@ -33,6 +35,6 @@ app.use('/api', apiRoutes);
 
 
 // start the server
-app.listen(3000, () => {
+app.listen(PORT, () => {
   console.log('Server is running on http://localhost:3000 or http://127.0.0.1:3000');
 });
